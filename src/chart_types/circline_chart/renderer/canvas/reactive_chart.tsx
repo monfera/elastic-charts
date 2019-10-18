c4e2f8a22b35f7aa4ad7ebdd7a65e5dbd57a075b
monfera <monfera.robert@gmail.com>
2019-10-19 00:30:30 +0200

    feat: removing stuff 7 - unneeded renderers

--- src/chart_types/circline_chart/renderer/canvas/reactive_chart.tsx
+++ src/chart_types/circline_chart/renderer/canvas/reactive_chart.tsx
@@ -4,14 +4,8 @@ import { connect, ReactReduxContext, Provider } from 'react-redux';
 import { ContainerConfig } from 'konva';
 import { Layer, Rect, Stage } from 'react-konva';
 import { BarGeometries } from './bar_geometries';
-import { LineGeometries } from './line_geometries';
-import { LineAnnotation } from './line_annotation';
-import { RectAnnotation } from './rect_annotation';
-import { Grid } from './grid';
 import { BarValues } from './bar_values';
 import { AnnotationDimensions } from '../../annotations/annotation_utils';
-import { AnnotationLineProps } from '../../annotations/line_annotation_tooltip';
-import { AnnotationRectProps } from '../../annotations/rect_annotation_tooltip';
 import { computeAnnotationDimensionsSelector } from '../../state/selectors/compute_annotations';
 import { computeChartTransformSelector } from '../../state/selectors/compute_chart_transform';
 import { getAnnotationSpecsSelector } from '../../state/selectors/get_specs';
@@ -20,7 +14,7 @@ import { isChartEmptySelector } from '../../state/selectors/is_chart_empty';
 import { isChartAnimatableSelector } from '../../state/selectors/is_chart_animatable';
 import { isBrushAvailableSelector } from '../../state/selectors/is_brush_available';
 import { Transform } from '../../state/utils';
-import { Rotation, AnnotationSpec, isLineAnnotation, isRectAnnotation } from '../../utils/specs';
+import { Rotation, AnnotationSpec } from '../../utils/specs';
 import { onChartRendered } from '../../../../state/actions/chart';
 import { isInitialized } from '../../../../state/selectors/is_initialized';
 import { getChartRotationSelector } from '../../../../state/selectors/get_chart_rotation';
@@ -28,7 +22,7 @@ import { getChartThemeSelector } from '../../../../state/selectors/get_chart_the
 import { GlobalChartState } from '../../../../state/chart_state';
 import { Dimensions } from '../../../../utils/dimensions';
 import { AnnotationId } from '../../../../utils/ids';
-import { Theme, mergeWithDefaultAnnotationLine, mergeWithDefaultAnnotationRect } from '../../../../utils/themes/theme';
+import { Theme } from '../../../../utils/themes/theme';
 import { LIGHT_THEME } from '../../../../utils/themes/light_theme';
 import { computeSeriesGeometriesSelector } from '../../state/selectors/compute_series_geometries';
 import { PointGeometry, BarGeometry, AreaGeometry, LineGeometry } from '../../../../utils/geometry';
@@ -97,73 +91,6 @@ class Chart extends React.Component<Props> {
     ];
   };
 
-  renderLineSeries = (clippings: ContainerConfig): ReactiveChartElementIndex[] => {
-    const { geometries, theme, isChartAnimatable, highlightedLegendItem } = this.props;
-    if (!geometries) {
-      return [];
-    }
-
-    const element = (
-      <LineGeometries
-        key={'line-geometries'}
-        animated={isChartAnimatable}
-        lines={geometries.lines || []}
-        sharedStyle={theme.sharedStyle}
-        highlightedLegendItem={highlightedLegendItem}
-        clippings={clippings}
-      />
-    );
-
-    return [
-      {
-        element,
-        zIndex: 0,
-      },
-    ];
-  };
-
-  renderAnnotations = (): ReactiveChartElementIndex[] => {
-    const { annotationDimensions, annotationSpecs } = this.props;
-    const annotationElements: ReactiveChartElementIndex[] = [];
-
-    annotationDimensions.forEach((annotation: AnnotationDimensions, id: AnnotationId) => {
-      const spec = annotationSpecs.find((spec) => spec.id === id);
-
-      if (!spec) {
-        return;
-      }
-
-      if (isLineAnnotation(spec)) {
-        const lineStyle = mergeWithDefaultAnnotationLine(spec.style);
-        const element = (
-          <LineAnnotation
-            key={`line-annotation-group-${id}`}
-            lines={annotation as AnnotationLineProps[]}
-            lineStyle={lineStyle}
-          />
-        );
-        annotationElements.push({
-          element,
-          zIndex: spec.zIndex || 0,
-        });
-      } else if (isRectAnnotation(spec)) {
-        const rectStyle = mergeWithDefaultAnnotationRect(spec.style);
-        const element = (
-          <RectAnnotation
-            key={`rect-annotation-group-${id}`}
-            rects={annotation as AnnotationRectProps[]}
-            rectStyle={rectStyle}
-          />
-        );
-        annotationElements.push({
-          element,
-          zIndex: spec.zIndex || 0,
-        });
-      }
-    });
-    return annotationElements;
-  };
-
   sortAndRenderElements() {
     const { chartDimensions, chartRotation } = this.props;
     const clippings = {
@@ -174,11 +101,7 @@ class Chart extends React.Component<Props> {
     };
 
     const bars = this.renderBarSeries(clippings);
-    const lines = this.renderLineSeries(clippings);
-    const annotations: ReactiveChartElementIndex[] = this.renderAnnotations();
-    return [...bars, ...lines, ...annotations]
-      .sort((elemIdxA, elemIdxB) => elemIdxA.zIndex - elemIdxB.zIndex)
-      .map((elemIdx) => elemIdx.element);
+    return [...bars].sort((elemIdxA, elemIdxB) => elemIdxA.zIndex - elemIdxB.zIndex).map((elemIdx) => elemIdx.element);
   }
 
   render() {
@@ -210,9 +133,7 @@ class Chart extends React.Component<Props> {
               {...brushProps}
             >
               <Provider store={store}>
-                <Layer hitGraphEnabled={false} listening={false}>
-                  <Grid />
-                </Layer>
+                <Layer hitGraphEnabled={false} listening={false} />
               </Provider>
               <Layer
                 x={chartDimensions.left + chartTransform.x}
