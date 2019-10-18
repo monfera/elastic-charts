52bc0b9a8d23aeba3b278c9aa54fd4182e898a10
monfera <monfera.robert@gmail.com>
2019-10-18 22:19:34 +0200

    feat: removing stuff

--- src/chart_types/circline_chart/specs/index.ts
+++ src/chart_types/circline_chart/specs/index.ts
@@ -1,6 +1,4 @@
-export { Axis } from './axis';
 export { LineAnnotation } from './line_annotation';
 export { RectAnnotation } from './rect_annotation';
 export { LineSeries } from './line_series';
-export { Circline } from './bar_series';
-export { AreaSeries } from './area_series';
+export { Circline } from './circline_series';
